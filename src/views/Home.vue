1799d9ce0817b3e5b4b176a51ec0483af9024fd6
zoutuan <zoutuan97@163.com>
2019-12-05 22:47:53 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <header>猫眼电影</header>
+      <main></main>
+      <footer></footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    
   }
 };
 </script>
